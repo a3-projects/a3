ceae823ca79a9b89bec80eed4dc251bc215211a4
oemer_aran@live.de <oemer_aran@live.de>
2023-10-28 22:47:37 +0200

    fix: remove netlify package for deployment

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +11,4 @@ export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
   }), sitemap(), react()],
-  adapter: netlify()
 });
