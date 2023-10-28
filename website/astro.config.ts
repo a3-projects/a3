3d7b5d5c7e06a93a71cbf415986ca4bdc88f9890
oemer_aran@live.de <oemer_aran@live.de>
2023-10-28 22:39:07 +0200

    chore: add netlify deployment

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://a3-team.de",
+  site: "https://magical-snickerdoodle-3d7fa3.netlify.app/",
   base: "",
-
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), react()],
+  adapter: netlify()
 });
