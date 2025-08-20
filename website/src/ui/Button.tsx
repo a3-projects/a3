abc29b4daffb7a2106033bb45d49ca041b25ab08
oemer.aran95@gmail.com <oemer.aran95@gmail.com>
2025-08-20 19:47:41 +0200

    feat: start reworking a3 website (wip)

--- website/src/ui/Button.tsx
+++ website/src/ui/Button.tsx
@@ -26,6 +26,7 @@ export const button = tv({
       default: "h-10 px-4 py-2",
       sm: "h-9  px-3",
       lg: "h-12 px-8",
+      xl: "h-16 px-10", // added xl size
       icon: "h-10 w-10",
     },
   },
